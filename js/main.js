e5334d8b229bd70c25aa2e31d00b2afaae810236
milan978gh <milan978@gmail.com>
2020-11-11 12:04:31 +0100

    update

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ about.addEventListener('click', function (e) {
     });
     // hide other articles
     articles.forEach(function (article) {
-      article.classList.remove(('active'));
+      article.classList.remove('active');
     });
     const element = document.getElementById(id);
     element.classList.add('active');
@@ -21,5 +21,22 @@ about.addEventListener('click', function (e) {
 });*/
 
 
+const about = document.querySelector('.about');
+const btns = document.querySelectorAll('.tab-btn');
+const articles = document.querySelectorAll('.content');
 
 
+about.addEventListener('click', function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    btns.forEach(function (btn) {
+      btn.classList.remove('active');
+      e.target.classList.add('active');
+    });
+    articles.forEach(function (article) {
+      article.classList.remove('active');
+    });
+    const element = document.getElementById(id);
+    element.classList.add('active');
+  }
+});
